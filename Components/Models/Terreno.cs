f0014cb204a3c778d62505e20274d890731e8535
FranCrujeiras <rodriguez.cr.fran1997@gmail.com>
2024-04-05 16:39:37 +0200

    Creación proyecto web
    Creamos los primeros pasos de la web:
     - Conectamos con la API creada
     - Añadimos el GET de Terrenos
     - Creamos un formulario, parametrizamos los valores de Terreno, y añadimos el POST que almacena los datos

---
+++ Components/Models/Terreno.cs
@@ -0,0 +1,26 @@
+﻿namespace FrontBlazor.Components.Models
+{
+    public class Terreno
+    {
+        public int Id { get; set; }        
+        public string ModeloPanel { get; set; }
+        public int Potencia { get; set; }
+        public double Voltaje { get; set; }
+        public double Latitud { get; set; }
+        public double Longitud { get; set; }
+        public double LargoTerreno { get; set; }
+        public double AnchoTerreno { get; set; }
+        public int Azimuth { get; set; }
+        public bool InstalacionEstructura { get; set; }
+        public int AnguloEstructura { get; set; }
+        public double? Separacion { get; set; }
+        public int? PotenciaTotal { get; set; }
+        public int? TotalPaneles { get; set; }
+
+        public Terreno()
+        {
+
+        }
+    }
+   
+}
