3d123cdd0f21879785dca67d93629b9104798827
FranCrujeiras <rodriguez.cr.fran1997@gmail.com>
2024-04-09 17:43:43 +0200

    09/04 Toqueteando la apariencia del formulario

--- obj/Debug/net8.0/FrontBlazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/FrontBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37ee33c239787374ce0c787b54c910414895d441")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a066a77cae3558e03874f81d311d975a8fa0aac0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
