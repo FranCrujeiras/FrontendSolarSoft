8f4d4f8c7db632fdc3244e65436f69d157a16738
FranCrujeiras <rodriguez.cr.fran1997@gmail.com>
2024-04-22 16:53:20 +0200

    22/04 Cambios listos hoja de resultados

--- obj/Debug/net8.0/FrontBlazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/FrontBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb7f2d097871ce2d006a5250d897cf384dc31c03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b8eb3229a98ff99c2888ad7f5b2611cb966ff37")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
